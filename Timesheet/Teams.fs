f030f7602d671ca51106e2001a31360d74447fa9
Radek Micek <radek.micek@gmail.com>
2021-04-10 13:28:08 +0200

    Store user ids of Teams chat members instead of member ids

--- Timesheet/Teams.fs
+++ Timesheet/Teams.fs
@@ -125,8 +125,11 @@ let listChats (client : GraphServiceClient) : Chat list =
             try
                 client.Me.Chats.[ch.Id].Members.Request()
                 |> getItems
-                // FIXME `m.Id` is not user id.
-                |> List.map (fun m -> { Id = m.Id; Name = m.DisplayName })
+                |> List.map (function
+                    | :? AadUserConversationMember as m -> { Id = m.UserId; Name = m.DisplayName }
+                    | m ->
+                        // `m.Id` is generally not user id.
+                        failwithf $"Cannot get user id of %s{m.DisplayName}, it has type: %A{m.GetType()}")
             with _ when ch.ChatType.Value = ChatType.Meeting ->
                 printfn $"Unable to list members of meeting %s{ch.Id} about %A{topic}"
                 []
