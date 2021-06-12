721d0be5ad3005ca9f8af8a798a71b10ff672f79
Radek Micek <radek.micek@gmail.com>
2021-06-12 15:53:59 +0200

    GitLab downloader retries if HTTP request fails

--- Timesheet/GitLab.fs
+++ Timesheet/GitLab.fs
@@ -1,5 +1,6 @@
 module GitLab
 
+open System.Threading
 open FSharp.Data
 
 type Projects = JsonProvider<"ApiResponseSamples/GitLabProjects.json">
@@ -11,17 +12,28 @@ type Changes = JsonProvider<"ApiResponseSamples/GitLabMergeRequestChanges.json">
 type Config = { ApiUrl: string; ApiToken: string }
 
 let request (config : Config) (path : string) =
-    let url = config.ApiUrl.TrimEnd('/') + "/v4/" + path.TrimStart('/')    
-    
+    let url = config.ApiUrl.TrimEnd('/') + "/v4/" + path.TrimStart('/')
+
     let headers = [
         "Private-Token", config.ApiToken
-        "Accept-Charset", "utf-8" ] 
-    let resp =
-        Http.Request(
-            url = url,
-            headers = headers,
-            // GitLab does not send encoding back.
-            responseEncodingOverride = "utf-8")
+        "Accept-Charset", "utf-8" ]
+    let rec getResponse () =
+        let resp =
+            try
+                Http.Request(
+                    url = url,
+                    headers = headers,
+                    // GitLab does not send encoding back.
+                    responseEncodingOverride = "utf-8")
+                |> Some
+            with e ->
+                printfn "Error '%s'. Will try again..." e.Message
+                Thread.Sleep 60_000
+                None
+        match resp with
+        | None -> getResponse ()
+        | Some resp -> resp
+    let resp = getResponse ()
 //    for h in resp.Headers do
 //        printfn "%A" h
     match resp.Body with
@@ -29,7 +41,7 @@ let request (config : Config) (path : string) =
     | _ -> failwithf "Unexpected response %A" resp
 
 let requestAllPagesParsed (config : Config) (path : string) (parseResp: string -> array<'T>) =
-    let paramSeparator = if path.Contains '?' then "&" else "?"    
+    let paramSeparator = if path.Contains '?' then "&" else "?"
     let perPage = 100
 
     let result = ResizeArray()
@@ -46,12 +58,12 @@ let requestAllPagesParsed (config : Config) (path : string) (parseResp: string -
             fetchPage (page + 1)
 
     fetchPage 1
-    result |> Seq.toList    
+    result |> Seq.toList
 
 let listProjectsOfCurrentUser config =
     let path = "projects?membership=true"
     let projects = requestAllPagesParsed config path Projects.Parse
-    projects 
+    projects
 
 let listMergeRequests config projId =
     let path = sprintf "projects/%d/merge_requests?scope=all" projId
@@ -59,7 +71,7 @@ let listMergeRequests config projId =
     mrs
 
 let listDiscussionsForMergeRequest config projId mrIid =
-    let path = sprintf "projects/%d/merge_requests/%d/discussions" projId mrIid    
+    let path = sprintf "projects/%d/merge_requests/%d/discussions" projId mrIid
     let discussions = requestAllPagesParsed config path Discussions.Parse
     discussions
 
@@ -68,18 +80,11 @@ let listEmoticonsForMergeRequest config projId mrIid =
     let emoticons = requestAllPagesParsed config path Emoticons.Parse
     emoticons
 
-let listChangesForMergeRequest config projId mrIid =
-    let path = sprintf "projects/%d/merge_requests/%d/changes" projId mrIid
-    let resp = request config path
-    let changes = Changes.Parse resp
-    changes
-
 type MergeRequest =
     { Proj : Projects.Root
       MR : MergeRequests.Root
       Discussions : list<Discussions.Root>
       Emoticons : list<Emoticons.Root>
-      Changes: Changes.Root
     }
 
 let fetchData (config : Config) =
@@ -90,11 +95,9 @@ let fetchData (config : Config) =
             for mr in mrs do
                 let discussions = listDiscussionsForMergeRequest config proj.Id mr.Iid
                 let emoticons = listEmoticonsForMergeRequest config proj.Id mr.Iid
-                let changes = listChangesForMergeRequest config proj.Id mr.Iid
                 yield { Proj = proj
                         MR = mr
                         Discussions = discussions
                         Emoticons = emoticons
-                        Changes = changes
                       }
     } |> Seq.toList
